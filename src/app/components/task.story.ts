551d854f9ebde8ec960d3ef423c645577d27b0bf
Totiruzi <totiruzi@yahoo.com>
2021-10-18 01:37:21 +0300

    Create task story properties

---
+++ src/app/components/task.story.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { action } from '@storybook/addon-actions';
+import { TaskComponent } from './task.component';
+
+export default {
+  component: TaskComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [TaskComponent],
+      imports: [CommonModule],
+    }),
+  ],
+  excludeStories: /.*Data$/,
+  title: 'Task',
+} as Meta;
+
+export const actionData = {
+  onPinTask : 'onPinTask',
+  onArchivedTask: 'onArchiveTask'
+};
+
+const Template: Story<TaskComponent> = args => ({
+  props: {
+    ...args,
+    onPinTask: actionData.onPinTask,
+    onArchivedTask: actionData.onArchivedTask,
+  },
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2921, 10, 18),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
