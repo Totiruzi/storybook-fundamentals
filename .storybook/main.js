b164c26218303ebf4a5a978e4176c2cade590572
Totiruzi <totiruzi@yahoo.com>
2021-10-19 13:54:27 +0300

    Add onPin, onArchived methods to TaskComponent

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "stories": ['../src/app/components/**/*.stories'],
+  "stories": ['../src/app/components/**/*.stories.ts'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -1,21 +1,53 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../model/task.model';
 
 
 @Component({
   selector: 'app-task',
   template: `
-    <div class="list-item">
-      <input type="text" [value]="task.title"  readonly="true"/>
+    <div class="list-item {{ task?.state }}">
+      <label class="checkbox">
+        <input
+          type="checkbox"
+          [defaultChecked]="task?.state === 'TASK_ARCHIVED'"
+          disabled="true"
+          name="checked"
+        />
+        <span class="checkbox-custom" (click)="onArchive(task.id)"></span>
+      </label>
+      <div class="title">
+        <input
+          type="text"
+          [value]="task?.title"
+          readonly="true"
+          placeholder="Input title"
+        />
+      </div>
+      <div class="actions">
+        <a *ngIf="task?.state !== 'TASK_ARCHIVED'" (click)="onPin(task.id)">
+          <span class="icon-star"></span>
+        </a>
+      </div>
     </div>
   `
 })
 
 export class TaskComponent {
-  @Input() task: any;
+  @Input() task: Task;
 
   // tslint:disable-next-line:no-output-on-prefix
   @Output() onPinTask = new EventEmitter<Event>();
 
   // tslint:disable-next-line:no-output-on-prefix
   @Output() onArchivedTask = new EventEmitter<Event>();
+
+  // component method to trigger the onPinEen @params() id: string;
+  onPin(id: any): void {
+    this.onPinTask.emit(id);
+  }
+
+  // component method to trigger the onArchivedEvent @params() id: string;
+  onArchived(id: any): void {
+    this.onArchivedTask.emit(id);
+  }
 }
